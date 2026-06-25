c190b9d8b4635c960ecbc25a211e9305ee992cdc
code-with-affoo <artzafnan@gmail.com>
2026-06-24 22:57:35 -0700

    Add files via upload

---
+++ module.py
@@ -0,0 +1,8 @@
+import pyjokes
+
+# print("Here's a random joke for you:")
+joke = pyjokes.get_joke()
+print(joke)
+"""
+so thanks that was my program that i made to get a random joke from the pyjokes library. it uses the get_joke() function to retrieve a joke and then prints it to the console. you can run this code in a python environment where the pyjokes library is installed to see a random joke each time you execute it.
+"""
